66b9ab06ce5d4f95b6eb7d5bae30d266a2433ccf
Alex Guadalupe <9035@holbertonstudents.com>
2024-07-19 22:49:04 -0400

    fix task 2 indentationa

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -5,28 +5,28 @@ export default class HolbertonCourse {
     if (!Array.isArray(students)) throw new TypeError('Students must be an array of strings');
     if (!students.every((student) => typeof student === 'string')) throw new TypeError('Students must be an array of strings');
 
-     this._name = name;
-     this._length = length;
-     this._students = students;
-    }
-  
-	// Getter and Setter name
-    get name() {
-    return this._name;
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+// Getter and Setter name
+  get name() {
+  return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+      this._name = name;
     }
 
-    set name(name) {
-     if (typeof name !== 'string') throw new TypeError('Name must be a string');
-      this._name = name;
-	}
-  
-	// Getter and Setter for length
-	get length() {
-	  return this._length;
-	}
-  
-	set length(length) {
-	  if (typeof length !== 'number') throw new TypeError('Length must be a number');
+// Getter and Setter for length
+  get length() {
+   return this._length;
+  }
+
+  set length(length) {
+if (typeof length !== 'number') throw new TypeError('Length must be a number');
 	  this._length = length;
 	}
   
