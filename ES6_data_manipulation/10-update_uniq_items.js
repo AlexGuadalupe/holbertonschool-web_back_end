46dd562947728927a828d173462a714a0af71eaf
Alex Guadalupe <9035@holbertonstudents.com>
2024-07-23 23:37:43 -0400

    task 10 done

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,12 @@
 export default function updateUniqueItems(map) {
   if (map instanceof Map) {
     map.forEach((value, key) => {
-        if (value === 1) {
-    map.set(key, 100);
+      if (value === 1) {
+        map.set(key, 100);
       }
     });
   } else {
-  throw new Error('Cannot process');
+    throw new Error('Cannot process');
   }
   return map;
 }
