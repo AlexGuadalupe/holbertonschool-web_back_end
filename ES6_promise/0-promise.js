636be386e5f25b8aed8c99d4a71b1d961f1de5f5
Alex Guadalupe <9035@holbertonstudents.com>
2024-07-25 21:05:57 -0400

    reset task 0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,12 +1,9 @@
-export default function getResponseFromApi(success = true) {
+export default function getResponseFromAPI(success = true) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
+      resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
-   }
+    }
   });
 }
